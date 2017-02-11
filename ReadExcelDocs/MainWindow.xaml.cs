c21df3084b59a4bea937df88e185e223080b8caf
raoasifraza11 <raoasifraza1@hotmail.com>
2017-02-12 00:43:53 +0500

    Create excel file from code

--- ReadExcelDocs/MainWindow.xaml.cs
+++ ReadExcelDocs/MainWindow.xaml.cs
@@ -1,72 +1,53 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ReadExcelDocs;
+using Excel = Microsoft.Office.Interop.Excel;
 namespace ReadExcelDocs
 {
-
-
-    using Excel = Microsoft.Office.Interop.Excel;
     
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        Excel.Application excelApp;
-        Excel.Workbook excelWorkBook;
-        Excel.Worksheet excelWorksheet;
-        Excel.Range range;
+        // Create a list of accounts.
+        Account data = new Account();
+        
 
         public MainWindow()
         {
             InitializeComponent();
-            excelApp = new Excel.Application();
         }
 
-        //private void btnOpenFile_Click(object sender, RoutedEventArgs e)
-        //{
-        //    OpenFileDialog openFileDialog = new OpenFileDialog();
-        //    //if (openFileDialog.ShowDialog() == true)
-        //      //  txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
-        //}
+        
 
         private void btnBrowes_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog filebrowes = new OpenFileDialog();
+            data.ID = 3456;
+            data.Balance = 546.24;
 
-            filebrowes.Filter = "Excel files (*.xlsx)|*.xlsx";
-            if (filebrowes.ShowDialog() == true)
-            {
-                string filepath = filebrowes.FileName;
+            var excelApp = new Excel.Application();
+            excelApp.Visible = true;
 
-                string text = "";
 
-                readExcelBook();
-                //MessageBox.Show(filename);
-            }
-        }
+            excelApp.Workbooks.Add();
 
-        private void readExcelBook()
-        {
-            excelWorkBook = excelApp.Workbooks.Open(@"C:\Users\Asif Raza\Desktop\excelbook.xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
-            excelWorksheet = (Excel.Worksheet)excelWorkBook.Sheets[1];
-            range = excelWorksheet.UsedRange;
-            //MessageBox.Show("@" + path);
+            Excel.Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
+
+            workSheet.Cells[1, "A"] = "4435";
+            workSheet.Cells[1, "B"] = "453";
+            workSheet.Columns[1].AutoFit();
+            workSheet.Columns[2].AutoFit();
+
 
         }
+
+      
+    }
+
+    // Account Class
+    public class Account
+    {
+        public int ID { get; set; }
+        public double Balance { get; set; }
     }
 }
