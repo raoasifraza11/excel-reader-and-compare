c93586bd7cdaed860a38383f99c82f81b7347e49
raoasifraza11 <raoasifraza1@hotmail.com>
2017-02-18 21:03:49 +0500

    New window for design UX

---
+++ ReadExcelDocs/MainWin.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ReadExcelDocs
+{
+    /// <summary>
+    /// Interaction logic for MainWin.xaml
+    /// </summary>
+    public partial class MainWin : Window
+    {
+        public MainWin()
+        {
+            InitializeComponent();
+        }
+    }
+}
