20bbc0775785c8a8b68b3b7a261d9f2c8a9ca7ec
raoasifraza11 <raoasifraza1@hotmail.com>
2017-02-20 00:47:03 +0500

    Backup

--- ReadExcelDocs/MainWin.xaml.cs
+++ ReadExcelDocs/MainWin.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System;
+﻿using System.Windows;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Win32;
+using System;
 
 namespace ReadExcelDocs
 {
@@ -19,9 +11,76 @@ namespace ReadExcelDocs
     /// </summary>
     public partial class MainWin : Window
     {
+        private string row, col;
         public MainWin()
         {
             InitializeComponent();
+            row = "D2";
+            col = "D50";
         }
+
+        private void syngbtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                run_excel_application();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Reading file", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void moddlebtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void comparebtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        /*
+         * open excel applicaiton to given path;
+         */ 
+        private void run_excel_application()
+        {
+            try
+            {
+                var excelApp = new Excel.Application { Visible = false };
+
+                excelApp.Workbooks.Open(@"E:\ee101synergy.xls");
+
+                Excel.Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
+
+                Excel.Range range = workSheet.Range[row, col];
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+             
+        }
+
+        private string file_browser()
+        {
+            OpenFileDialog getfile = new OpenFileDialog { Filter = "Excel files(*.xlsx) | *.xlsx" };
+            
+
+            if(getfile.ShowDialog() == true)
+            {
+                return getfile.FileName;   
+            }else
+            {
+                return null;
+            }
+        }
+
+
     }
 }
