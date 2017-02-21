0ccedb8960ff23fe88b3c329ce5161c0184c0312
raoasifraza11 <raoasifraza1@hotmail.com>
2017-02-22 00:02:24 +0500

    Open application handle range.

--- ReadExcelDocs/TempProgram.xaml.cs
+++ ReadExcelDocs/TempProgram.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ReadExcelDocs
 {
@@ -19,14 +20,32 @@ namespace ReadExcelDocs
     /// </summary>
     public partial class TempProgram : Window
     {
+        // Required variables
+        private string synRow, synCol, modRow, modCol;
+
         public TempProgram()
         {
             InitializeComponent();
+            // Intilize local variables or Entities
+            synRow = synRangeRow.Text.ToString();
+            synCol = synRangeCol.Text.ToString();
+            modRow = modCol = null;
         }
 
         private void synergybtn_Click(object sender, RoutedEventArgs e)
         {
             synTittle.Text = Core.getFile();
+
+            var excelApp = new Excel.Application { Visible = false };
+            excelApp.Workbooks.Open(@"E:\EE101spring2017_Attendances_20170131-0006.xlsx");
+            Excel.Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
+
+            string row = synRow;
+            string col = synCol;
+            //string[] SelectedRange = { synRangeRow.Text.ToString(), synRangeCol.Text.ToString() };
+
+            Excel.Range range = workSheet.Range[row, col];
+            
         }
 
         private void moodlebtn_Click(object sender, RoutedEventArgs e)
