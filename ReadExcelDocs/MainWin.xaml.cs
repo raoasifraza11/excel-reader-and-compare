a89e15a22672912841eeb40e330be933e747685c
raoasifraza11 <raoasifraza1@hotmail.com>
2017-02-21 20:54:29 +0500

    Create New Interface

--- ReadExcelDocs/MainWin.xaml.cs
+++ ReadExcelDocs/MainWin.xaml.cs
@@ -12,10 +12,15 @@ namespace ReadExcelDocs
     public partial class MainWin : Window
     {
         //private Excel.Application excelApp;
+        private List<string> data1;
+        private List<string> data2;
 
         public MainWin()
         {
             InitializeComponent();
+            data1 = new List<string>();
+            data2 = new List<string>();
+            listView.Visibility = Visibility.Hidden;
             //excelApp = new Excel.Application { Visible = false };
         }
 
@@ -23,7 +28,7 @@ namespace ReadExcelDocs
         {
             try
             {
-                var excelApp = new Excel.Application { Visible = true };
+                var excelApp = new Excel.Application { Visible = false };
 
                 excelApp.Workbooks.Open(@"E:\ee101synergy.xls");
 
@@ -46,8 +51,7 @@ namespace ReadExcelDocs
                     {
                         str = (string)((range.Cells[rCnt, cCnt] as Excel.Range).Value2).ToString().Substring(15);
                         //output.Text += str + "\n";
-                        //data1.Add(str);
-                        MessageBox.Show(str);
+                        data1.Add(str);
                     }
                 }
             }
@@ -59,12 +63,43 @@ namespace ReadExcelDocs
 
         private void moddlebtn_Click(object sender, RoutedEventArgs e)
         {
+            var excelApp = new Excel.Application { Visible = false };
+
+            excelApp.Workbooks.Open(@"E:\EE101spring2017_Attendances_20170131-0006.xlsx");
+
+            Excel.Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
+            Excel.Range range = workSheet.Range[moodRow.Text.ToString(), moodCol.Text.ToString()];
+
+            int rc = range.Rows.Count;
+            int cc = range.Columns.Count;
+
+            int rCnt, cCnt;
+            string str;
+            //output_2.Text = string.Empty;
+            // int i; no need more
+            for (rCnt = 1; /* i = 0 */ rCnt <= rc; rCnt++)
+            {
+                for (cCnt = 1; cCnt <= cc; cCnt++)
+                {
+                    str = (string)((range.Cells[rCnt, cCnt] as Excel.Range).Value2).ToString();
+                    //output_2.Text += str + "\n";
+                    data2.Add(str);
+                }
+            }
 
         }
 
         private void comparebtn_Click(object sender, RoutedEventArgs e)
         {
-
+            List<string> notfound = new List<string>();
+            foreach (var item1 in data1)
+            {
+                if (!(data2.Contains(item1)))
+                {
+                    notfound.Add(item1);
+                }
+            }
+            listView.ItemsSource = notfound;
         }
 
 
@@ -85,6 +120,7 @@ namespace ReadExcelDocs
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+
         }
 
         private string file_browser()
@@ -101,6 +137,9 @@ namespace ReadExcelDocs
             }
         }
 
-
+        private void showresultbtn_Click(object sender, RoutedEventArgs e)
+        {
+            listView.Visibility = Visibility.Visible;
+        }
     }
 }
