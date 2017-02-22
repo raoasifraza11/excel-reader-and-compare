d8803ac64d7d5be2b4240bfb8497cd5f104cc24d
raoasifraza11 <raoasifraza1@hotmail.com>
2017-02-22 09:42:39 +0500

    Delete unsued resourses

--- ReadExcelDocs/MainWindow.xaml.cs
+++ ReadExcelDocs/MainWindow.xaml.cs
@@ -1,145 +1,118 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 using Excel = Microsoft.Office.Interop.Excel;
+
 namespace ReadExcelDocs
 {
-    
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for TempProgram.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TempProgram : Window
     {
-        // Create a list of accounts.
-        Account data = new Account();
-        private string row, col;
-        private List<string> data1;
-        private List<string> data2;
+        // Required variables
+        //private string synRow, synCol, modRow, modCol;
+        private List<string> synData;
+        private List<string> moodleData;
+        List<string> compareOneToTwo;
+        List<string> compareTwoToOne;
 
-        public MainWindow()
+        public TempProgram()
         {
             InitializeComponent();
-            row = "D2";
-            col = "D50";
-            data1 = new List<string>();
-            data2 = new List<string>();
+            // Intilize local variables or Entities
+            //synRow = synCol = modRow = modCol = null;
+            synData = new List<string>();
+            moodleData = new List<string>();
+            compareOneToTwo = new List<string>();
+            compareTwoToOne = new List<string>();
+        }
 
+        private void synergybtn_Click(object sender, RoutedEventArgs e)
+        {
+            synTittle.Text = Core.getFilePath().ToString();
 
+            OpenExcelApplication(synTittle.Text, synData);
 
         }
 
-        
-
-        private void btnBrowes_Click(object sender, RoutedEventArgs e)
+        private void moodlebtn_Click(object sender, RoutedEventArgs e)
         {
-           data.ID = 3456;
-            data.Balance = 546.24;
-
-            var excelApp = new Excel.Application();
-            excelApp.Visible = false;
-
-            excelApp.Workbooks.Add();
-
-            Excel.Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
-
-            workSheet.Cells[1, "A"] = "4435";
-            workSheet.Cells[1, "B"] = "453";
-            workSheet.Columns[1].AutoFit();
-            workSheet.Columns[2].AutoFit();
-
-
+            moodleTitle.Text = Core.getFilePath().ToString();
+            OpenExcelApplication(moodleTitle.Text, moodleData);
         }
 
-        private void excelbtn_Click(object sender, RoutedEventArgs e)
+        private void viewResult_Click(object sender, RoutedEventArgs e)
         {
-            var excelApp = new Excel.Application { Visible = true };
+            Result r = new Result();
+            r.Show();
+            r.result1Listview.ItemsSource = synData;
+            r.moodleListView.ItemsSource = moodleData;
 
-            excelApp.Workbooks.Open(@"E:\ee101synergy.xls");
+            r.compareResult1to2.ItemsSource = compareOneToTwo;
+            r.compareResult2to1.ItemsSource = compareTwoToOne;
+        }
 
-            Excel.Worksheet workSheet = (Excel.Worksheet) excelApp.ActiveSheet;
 
-            Excel.Range range = workSheet.Range[row,col];
+        private void OpenExcelApplication(string path, List<string> data)
+        {
+            try
+            {
+                var excelApp = new Excel.Application { Visible = false };
+                excelApp.Workbooks.Open(path);
+                Excel.Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
 
-            int rc = range.Rows.Count;
-            int cc = range.Columns.Count;
+                string row = synRangeRow.Text.ToString();
+                string col = synRangeCol.Text.ToString();
+                //string[] SelectedRange = { synRangeRow.Text.ToString(), synRangeCol.Text.ToString() };
+
+                Excel.Range range = workSheet.Range[row, col];
+                for (int i = 1; i <= range.Rows.Count; i++)
+                {
+                    for (int j = 1; j <= range.Columns.Count; j++)
+                    {
+                        data.Add((string)((range.Cells[i, j]
+                        as Excel.Range).Value2).ToString());
+                    }
+                }
+
+                excelApp.Workbooks.Close();
+                excelApp.Application.Quit();
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK);
+            }
             
-            int rCnt, cCnt;
-
-            //
-
-            //string a = "ABCD-0448";
-            //string required = a.Substring(5);
-
-            string str;
-            //output.Text = string.Empty;
-            for (rCnt = 1; rCnt <= rc; rCnt++)
-            {
-                for (cCnt = 1; cCnt <= cc; cCnt++)
-                {
-                    str = (string)((range.Cells[rCnt, cCnt] as Excel.Range).Value2).ToString().Substring(15);
-                    //output.Text += str + "\n";
-                    data1.Add(str);
-                }
-            }
-            listView.ItemsSource = data1;
-            // excelApp.Workbooks.Close();
-            //excelApp.Application.Quit();
         }
 
-        private void comparebtn_Click(object sender, RoutedEventArgs e)
+        private void compare_Click(object sender, RoutedEventArgs e)
         {
-            List<string> notfound = new List<string>();
-            foreach(var item1 in data1)
+            foreach (var item in synData)
             {
-                if (!(data2.Contains(item1)))
+                if (!(moodleData.Contains(item)))
                 {
-                    notfound.Add(item1);
+                    compareOneToTwo.Add(item);
                 }
             }
 
-            listView2.ItemsSource = notfound;
-
-        }
-
-        private void second_Click(object sender, RoutedEventArgs e)
-        {
-            var excelApp = new Excel.Application { Visible = false };
-
-            excelApp.Workbooks.Open(@"E:\EE101spring2017_Attendances_20170131-0006.xlsx");
-
-            Excel.Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
-            row = "B5";
-            col = "B53";
-            Excel.Range range = workSheet.Range[row, col];
-
-            int rc = range.Rows.Count;
-            int cc = range.Columns.Count;
-
-            int rCnt, cCnt;
-            string str;
-            //output_2.Text = string.Empty;
-            // int i; no need more
-            for (rCnt = 1; /* i = 0 */ rCnt <= rc; rCnt++)
+            foreach (var item in moodleData)
             {
-                for (cCnt = 1; cCnt <= cc; cCnt++)
+                if (!(synData.Contains(item)))
                 {
-                    str = (string)((range.Cells[rCnt, cCnt] as Excel.Range).Value2).ToString();
-                    //output_2.Text += str + "\n";
-                    data2.Add(str);
+                    compareTwoToOne.Add(item);
                 }
             }
 
-            listView1.ItemsSource = data2;
-            // don't need to close workbook
-            //excelApp.Workbooks.Close();
-
-            excelApp.Application.Quit();
         }
-    }
-
-    // Account Class
-    public class Account
-    {
-        public int ID { get; set; }
-        public double Balance { get; set; }
     }
 }
